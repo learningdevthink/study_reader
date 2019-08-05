47408d68cfd2d83b76f034cda293b24f404d7289
namedboy <sjkim@artifriends.com>
2019-08-05 21:33:15 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
-print("hello world")
-print("hello" * 5)
-print("")
+input_str = input("숫자를 입력해라! > ")
+input_num = int(input_str)
+
+if input_num == 3:
+    print(input_num)
+    print(type(input_num))
+    print("너는 3을 입력하였구나!!")
+else:
+    print(input_num)
+    print(type(input_num))
+    print("너는 3이 아닌 값을 입력하였구나!")
+
+
